3680a49c87130ab5269d3823bd8e6e08e4606804
DarkSearinoX <ferodriguez.mbl@gmail.com>
2013-03-26 15:35:42 -0400

    Added Sequence search type, form themes and controller section

--- src/FragProject/FragBundle/Controller/SearchController.php
+++ src/FragProject/FragBundle/Controller/SearchController.php
@@ -2,9 +2,11 @@
 
 namespace FragProject\FragBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use FragProject\FragBundle\Form\SequenceSearchType;
 
 /**
  * 
@@ -14,12 +16,34 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class SearchController extends Controller
 {
     /**
-     * @Route("/",name="search_home")
+     * @Route("/",name="fragprot_search_home")
      * @Template()
      */
     public function indexAction()
     {
-        
         return array();
     }
+    
+    /**
+     * @Route("/seq_search",name="fragprot_seq_search_home")
+     * @Template()
+     */
+    public function seqSearchAction(Request $request)
+    {
+        $data = array();
+        
+        $data['sequence']='';
+        
+        $form = $this->createForm(new SequenceSearchType(), $data);
+        
+        if ($this->get('request')->isMethod('get') && $this->get('request')->get('PIDateRange')) {
+            $form->bind($this->get('request'));
+            $data['sequence'] = $form->get('sequence')->getData();
+        }
+        
+        return array(
+            'form'=>$form->createView()
+        );
+    }
+    
 }
