e5650cf4412e61b85a814928b85a88745aae13f0
DarkSearinoX <ferodriguez.mbl@gmail.com>
2013-06-02 23:32:39 -0400

    added load fragment fixture, ToDo: load fragment data in fixture

--- src/MeloLab/FragProt/WebBundle/DataFixtures/ORM/LoadFragmentData.php
+++ src/MeloLab/FragProt/WebBundle/DataFixtures/ORM/LoadFragmentData.php
@@ -11,7 +11,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * Load aminoacid data into the database.
  * @author Felipe Rodriguez <ferodriguez.mbl@gmail.com>
  */
-class LoadAminoacidData implements FixtureInterface, ContainerAwareInterface
+class LoadFragmentData implements FixtureInterface, ContainerAwareInterface
 {
     /**
      * @var ContainerInterface
@@ -36,16 +36,8 @@ class LoadAminoacidData implements FixtureInterface, ContainerAwareInterface
         $tableName = $em->getClassMetadata('MeloLabEvaBundle:Fragment')->getTableName();
         $em->getConnection()->exec("ALTER TABLE $tableName AUTO_INCREMENT = 1;");
 
-        /* 01 */ 
-        $e = new Aminoacid(); 
-        $e->setFullName('Alanine');
-        $e->setThreeLetterAminoacid('Ala');
-        $e->setOneLetterAminoacid('A');
-        $e->setHidrophCw(0.66);
-        $e->setHidrophB(0.169);
-        $manager->persist($e);
-        
-        
+        /* 01 */ $e = new Aminoacid(); $e->setFullName('Full Scholarship'); $manager->persist($e);
+
 
         $manager->flush();
     }
